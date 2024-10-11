49771ca95b3a3c48c14a016759be10ff6fc053ef
Lola <lolamartineztorassi6@gmail.com>
2024-10-11 13:06:39 -0300

    manejo de errores y archivos

--- data/calculadora.py
+++ data/calculadora.py
@@ -10,10 +10,17 @@ def calculos(operador, num1, num2, historial):
     else:
         return print("Operacion no valida")
     operacion = (f"{num1} {operador} {num2} = {resultado}")
-    historial.append(operacion)
+    guardar_operacion(operacion)
     return operacion
-def mostrar_historial(historial):
-    return "/n".join(historial)
+def guardar_operacion(operacion):
+    with open("historial.txt", "a") as historial:
+        historial.write(operacion + "/n")
+def mostrar_historial():
+    try:
+        with open("historial.txt", "r") as historial:
+            return historial.read()
+    except:
+        return "El historial esta vacio"
 def calculadora():
     historial = []
 
@@ -27,22 +34,25 @@ def calculadora():
         opcion = input("Elige una opcion: ")
 
         if opcion in ["1", "2", "3", "4"]:
-            num1 = float(input("Ingrese el primer numero: "))
-            num2 = float(input("Ingrese el segundo numero: "))
+            try:
+                num1 = float(input("Ingrese el primer numero: "))
+                num2 = float(input("Ingrese el segundo numero: "))
                 
-            if opcion == "1":
-                operador = "+"
-            elif opcion == "2":
-                operador = "-"
-            elif opcion == "3":
-                operador = "/"
-            elif opcion == "4":
-                operador = "*"
-            print(calculos(operador, num1, num2, historial))
-            continue
+                if opcion == "1":
+                    operador = "+"
+                elif opcion == "2":
+                    operador = "-"
+                elif opcion == "3":
+                    operador = "/"
+                elif opcion == "4":
+                    operador = "*"
+                print(calculos(operador, num1, num2, historial))
+                continue
+            except:
+                print("Por favor ingrese numeros.")
         elif opcion == "5":
             print("\nHistorial de operaciones:")
-            print(mostrar_historial(historial))
+            print(mostrar_historial())
         elif opcion == "6":
             print("Saliste de la calculadora")
             break
